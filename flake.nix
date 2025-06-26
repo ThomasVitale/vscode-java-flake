7ccce681da01f5da996e64c458211a4153a8cf6f
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-06-26 20:34:55 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -15,13 +15,29 @@
         };
 
         vscode-with-extensions = pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
           vscodeExtensions = with pkgs.vscode-extensions; [
             redhat.java
             redhat.vscode-yaml
             vscjava.vscode-java-debug
             vscjava.vscode-java-test
             vscjava.vscode-java-dependency
+            vscjava.vscode-spring-initializr
             visualstudioexptteam.vscodeintellicode
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "vscode-spring-boot";
+              publisher = "vmware";
+              sha256 = "sha256-ILUyJkmkGyiFxPNTPxix3GppEumA/WDu9XUOPM6RSvs=";
+              version = "1.63.0";
+            }
+            {
+              name = "vscode-spring-boot-dashboard";
+              publisher = "vscjava";
+              sha256 = "sha256-AwcJrCjKjlBi5McCbr0MGIBi3gIQn4mtcq5eK85YylQ=";
+              version = "0.14.0";
+            }
           ];
         };
       in
